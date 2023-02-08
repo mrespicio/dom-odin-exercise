01a2b445a8a38555630fbdcb320066c54ce5cc3c
mrespicio <meganrespicio@hotmail.com>
2023-02-08 13:52:13 -0800

    complete first exercise of dom manipulation

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const text = document.createElement('p');
-text.style.color = 'red';
+const body = document.body;
+const container = document.getElementById('container');
 
-body.text = "Hello I'm Red!";
-body.append(text);
+//const text = "<h1>Hello World</h>"
+//outerContainer.innerHTML = text;
 
-const container = document.querySelector('#container');
-container.append("I've appended to the container")
+const redText = document.createElement('p');
+redText.textContent = "Hi! I'm red!";
+redText.style.color = 'red';
+container.append(redText);
+
+const blueText = document.createElement('h3');
+blueText.textContent = "I'm a blue h3!";
+blueText.style.color = 'blue';
+container.append(blueText);
+
+const newDiv = document.createElement('div');
+newDiv.setAttribute('style', 'border: solid black 1px; background: pink')
+
+const divHeadOne = document.createElement('h1');
+divHeadOne.textContent = "I'm in a div";
+newDiv.append(divHeadOne);
+
+const divPara = document.createElement('p');
+divPara.textContent = "ME TOO!";
+newDiv.append(divPara);
+
+container.append(newDiv);
